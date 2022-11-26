713d248311458c5af107bb8f2bc8c6aacef32f2c
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-26 16:28:03 +0600

    mongodb added and create two collection

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 const app = express();
-
 
 // middleware 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.g8htdaf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('collectedMobile').collection('categories');
+        const productsCollection = client.db('collectedMobile').collection('products');
+
+        
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.log(error))
+
+app.get('/', async (req, res) => {
+    res.send('Collected Mobile server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Collected Mobile server running on ${port}`);
+})
